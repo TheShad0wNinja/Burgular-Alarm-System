b664c05807592b4443dd46f2e7ee3f6121ef0e65
Mohey Karam <moheykaramdev@gmail.com>
2026-04-13 18:22:21 +0200

    First Commit :3

---
+++ src/bas/core/BurgularAlarmSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bas.core;
+
+/**
+ *
+ * @author shadow
+ */
+public class BurgularAlarmSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+    
+}
